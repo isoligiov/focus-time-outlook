a06467504fb877f80c30d32dafdc932b448fce06
mac <mac@SNYPER-MAC.local>
2025-04-07 18:30:32 -0400

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+const PROJECT_NAME = 'BD';
+const SENSITIVE_CHANNELS = [
+];
+
+let previousState = {};
+let initialState = true;
+
+(async () => {
+  while(true) {
+    try {
+      const currentState = {}
+      const markAsReadButtons = document.querySelectorAll("div[data-app-section='MessageList'] button[title='Mark as read']")
+      for(let markAsReadButton of markAsReadButtons) {
+        try {
+          let parent = markAsReadButton.parentElement
+          while(parent && parent.getAttribute('role') !== "option")
+            parent = parent.parentElement
+          if(!parent) break
+          const labels = parent.querySelectorAll('span[title]')
+          const header = labels[0].textContent.trim()
+          const preview = labels[1].textContent.trim()
+          const timestamp = labels[2].textContent.trim()
+          currentState[header] = { timestamp, preview }
+        } catch(err) {}
+      }
+      let text = ''
+      for(let header in currentState) {
+        const preview = currentState[header].preview
+        if(preview !== previousState[header]?.preview) {
+          const previewText = (preview || "").replace('`', '"').split(/[\r\n]/g).filter(line => line.length > 0).map(line => `\`${line}\``).join("\n")
+          text += `*${header} @ ${PROJECT_NAME}* sent email!
+${previewText}`
+        }
+      }
+      if(text.length > 0) {
+        await chrome.runtime.sendMessage({
+          message_type: 'sendUpdate',
+          text,
+        })
+      }
+      previousState = currentState
+    } catch(err) {
+    }
+    await sleep(1000)
+  }
+})()
---
+++ scripts/service-worker.js
@@ -0,0 +1,39 @@
+const SERVER_URL = 'http://localhost:5031'
+
+async function sendUpdate({text}) {
+  const response = await fetch(SERVER_URL, {
+    method: 'POST',
+    body: JSON.stringify({ type: 'notification', text }),
+    headers: new Headers({'content-type': 'text/plain'}),
+  })
+  if(response.status != 200)
+    throw new Error('api request failed')
+  return response
+}
+
+const messageHandlerMap = {
+  sendUpdate,
+}
+
+// Message handler for all incoming messages within the chrome extension
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const messageType = request.message_type
+  if(messageHandlerMap[messageType]) {
+    (async() => {
+      try {
+        const data = { ...request }
+        const response = await messageHandlerMap[messageType](data)
+        sendResponse({
+          success: true,
+          ...response
+        })
+      } catch(error) {
+        sendResponse({
+          success: false,
+          error: JSON.stringify(error)
+        })
+      }
+    })()
+    return true
+  }
+})
